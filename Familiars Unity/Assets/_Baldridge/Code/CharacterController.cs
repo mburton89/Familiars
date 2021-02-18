d86b24fa25c972442a0939737eb17d5129122ae3
JustinBaldridge <justinbaldridge@gmail.com>
2021-02-17 19:09:57 -0600

    character can move

---
+++ Familiars Unity/Assets/_Baldridge/Code/CharacterController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private const float MOVE_SPEED = 6f;
+
+    [SerializeField] private LayerMask dashLayerMask;
+
+    private Rigidbody2D rigidbody2D;
+    private Vector3 moveDir;
+    private bool isDashButtonDown;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float moveX = 0f;
+        float moveY = 0f;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            moveY = +1f;
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            moveY = -1f;
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            moveX = -1f;
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            moveX = +1f;
+        }
+
+        moveDir = new Vector3(moveX, moveY).normalized;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            isDashButtonDown = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.velocity = moveDir * MOVE_SPEED;
+
+        if(isDashButtonDown)
+        {
+            float dashAmount = 4f;
+            Vector3 dashPosition = transform.position + moveDir * dashAmount;
+
+            RaycastHit2D raycastHit2d = Physics2D.Raycast(transform.position, moveDir, dashAmount, dashLayerMask);
+            if(raycastHit2d.collider != null)
+            {
+                dashPosition = raycastHit2d.point;
+            }
+
+            rigidbody2D.MovePosition(dashPosition);
+            isDashButtonDown = false;
+        }
+    }
+}
