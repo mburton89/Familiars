24ba0f373bbb12436759882a1537b4ef90051a18
Unclearfall626 <77085144+Unclearfall626@users.noreply.github.com>
2021-03-25 18:29:50 -0500

    MusicTest

---
+++ Familiars Unity/Assets/_Smith/GameMusic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Music", menuName = "Familiar/Music")]
+public class GameMusic : ScriptableObject
+{
+    public static GameMusic Instance;
+
+    public AudioClip mapMusic;
+    public AudioClip mapMusic2;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+}
