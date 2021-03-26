3446bb11786ef68d6543718115c0a499afff9d39
Unclearfall626 <77085144+Unclearfall626@users.noreply.github.com>
2021-03-25 19:05:45 -0500

    Audio Stuff

--- Familiars Unity/Assets/_Smith/GameMusic.cs
+++ Familiars Unity/Assets/_Smith/GameMusic.cs
@@ -10,6 +10,35 @@ public class GameMusic : ScriptableObject
     public AudioClip mapMusic;
     public AudioClip mapMusic2;
 
+    public AudioClip menuMusic1;
+    public AudioClip menuMusic2;
+    public AudioClip menuMusic3;
+
+    public AudioClip angeredmcSFX;
+    public AudioClip curiousmcSFX;
+    public AudioClip hmmmcSFX;
+    public AudioClip huhmcSFX;
+    public AudioClip scaredmcSFX;
+
+    public AudioClip attackmvSFX;
+    public AudioClip evilchucklemvSFX;
+    public AudioClip hmmmvSFX;
+    public AudioClip injuredshockmvSFX;
+    public AudioClip injuredmvSFX;
+    public AudioClip shockmvSFX;
+    public AudioClip sighmvSFX;
+    public AudioClip smirkmvSFX;
+
+    public AudioClip injuredgvSFX;
+    public AudioClip injured2gvSFX;
+    public AudioClip evilchucklegvSFX;
+
+    public AudioClip attackfrSFX;
+    public AudioClip laughfrSFX;
+    public AudioClip shockfrSFX;
+    public AudioClip hmmfrSFX;
+    public AudioClip injuredfrSFX;
+
     void Awake()
     {
         Instance = this;
